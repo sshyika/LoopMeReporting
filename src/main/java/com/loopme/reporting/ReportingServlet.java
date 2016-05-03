c4f78fd3a70d5faef1d2b3bf8ce8fb397dd6018c
Sergey Shyika <sshyika@gmail.com>
2016-05-03 20:38:07 +0300

    no message

--- src/main/java/com/loopme/reporting/ReportingServlet.java
+++ src/main/java/com/loopme/reporting/ReportingServlet.java
@@ -7,32 +7,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/report"}, loadOnStartup = 1)
+@WebServlet(urlPatterns = {"/api/*"}, loadOnStartup = 1)
 public class ReportingServlet extends HttpServlet {
 
     private Dictionary dictionary;
 
     @Override
     public void init() throws ServletException {
-        dictionary = DashboardDao.loadDictionary();
+        //dictionary = DashboardDao.loadDictionary();
     }
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setHeader("Content-type", "text/plain; charset=UTF-8");
-        dictionary
-                .getApps()
-                .entrySet()
-                .stream()
-                .map(e -> e.getKey() + " : " + e.getValue() + "\n")
-                .forEach(e -> {
-                    try {
-                        resp.getWriter().write(e);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                });
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        String content = "";
+        switch (req.getPathInfo()) {
+            case "/report" :
+                content = getReport(req);
+                break;
+        }
+        resp.getWriter().write(content);
+    }
+
+
+    private String getReport(HttpServletRequest req) {
+        return "[\n" +
+                "    {\n" +
+                "        \"name\": \"Tiger Nixon\",\n" +
+                "        \"position\": \"System Architect\",\n" +
+                "        \"salary\": \"$320,800\",\n" +
+                "        \"start_date\": \"2011/04/25\",\n" +
+                "        \"office\": \"Edinburgh\",\n" +
+                "        \"extn\": \"5421\"\n" +
+                "    }" +
+                "]";
     }
 
 
