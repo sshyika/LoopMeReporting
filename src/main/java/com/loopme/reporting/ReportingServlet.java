4d9a59199da7b299082fcf99b49cb3bbe52df67a
Sergey Shyika <sshyika@gmail.com>
2016-05-03 21:00:29 +0300

    no message

--- src/main/java/com/loopme/reporting/ReportingServlet.java
+++ src/main/java/com/loopme/reporting/ReportingServlet.java
@@ -35,12 +35,23 @@ public class ReportingServlet extends HttpServlet {
     private String getReport(HttpServletRequest req) {
         return "[\n" +
                 "    {\n" +
-                "        \"name\": \"Tiger Nixon\",\n" +
-                "        \"position\": \"System Architect\",\n" +
-                "        \"salary\": \"$320,800\",\n" +
-                "        \"start_date\": \"2011/04/25\",\n" +
-                "        \"office\": \"Edinburgh\",\n" +
-                "        \"extn\": \"5421\"\n" +
+                "      \"ad_clicks\": 2380,\n" +
+                "      \"bid_wins\": 0,\n" +
+                "      \"earnings_cents\": 1140000000,\n" +
+                "      \"video_starts\": 25215,\n" +
+                "      \"b_video_completes\": 0,\n" +
+                "      \"tracking\": 0,\n" +
+                "      \"app_price_cents\": 0,\n" +
+                "      \"ad_viewable\": 34224,\n" +
+                "      \"app_confirmed_installs\": 4,\n" +
+                "      \"ad_success\": 543336,\n" +
+                "      \"inbox_opens\": 41011,\n" +
+                "      \"a_price_cents\": 0,\n" +
+                "      \"video_completes\": 0,\n" +
+                "      \"campaign_id\": \"7358\",\n" +
+                "      \"b_clicks\": 240,\n" +
+                "      \"b_views\": 0,\n" +
+                "      \"ad_views\": 40775\n" +
                 "    }" +
                 "]";
     }
